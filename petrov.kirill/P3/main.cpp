296f7f236123464fa57aba9774620c6681c83c79
k1r1ll <k1r1ll@k1r1ll.k1r1ll>
2025-11-04 21:10:31 +0300

    считывание массива

--- petrov.kirill/P3/main.cpp
+++ petrov.kirill/P3/main.cpp
@@ -1,7 +1,51 @@
+#include <fstream>
 #include <iostream>
+#include <limits>
 
-int main()
+namespace petrov
 {
-  std::cout << "petrov.kirill\n";
+    int isitnumber(const char* a);
+    int isitoneortwo(const char* a);
 }
 
+int petrov::isitnumber(const char* a)
+{
+    int i = 0;
+    while (a[i] != '\0')
+    {
+        if (a[i] > '9' || a[i] < '0')
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int petrov::isitoneortwo(const char* a){
+    return ((a[0] == '1' || a[0] == '2') && a[1] == '\0') ? 1 : 0;
+}
+
+int main(int argc, char ** argv)
+{
+    if (argc < 4)
+    {
+        std::cerr << "Not enough arguments\n";
+        return 1;
+    }
+    else if (argc > 4)
+    {
+        std::cerr << "Too many arguments\n";
+        return 1;
+    }
+    else if(!petrov::isitnumber(argv[1]))
+    {
+        std::cerr << "First parameter is not a number\n";
+        return 1;
+    }
+    else if(!petrov::isitnumber(argv[1]))
+    {
+        std::cerr << "First parameter is out of range\n";
+        return 1;
+    }
+}
